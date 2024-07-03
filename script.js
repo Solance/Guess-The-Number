74d9c0dedec4c03d436412391411038c9056bfb1
Dana Holm <dana.holm84@gmail.com>
2024-07-03 10:47:35 +0200

    storing and displaying guessed numbers list

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let randomizedNumber;
 let guessedNumber;
+let guessedNumbers = [];
+const guesses = document.querySelector(".guesses");
 
 function randomizeNumber() {
     randomizedNumber = Math.floor(Math.random() * 100) + 1;
@@ -7,9 +9,15 @@ function randomizeNumber() {
 randomizeNumber();
 console.log(randomizedNumber);
 
-function getGuessedNumberVal() {
+function submitGuessedNumber() {
     guessedNumber = Number(document.getElementById("number-input").value);
     checkNumber(guessedNumber);
+    guessedNumbers.push(guessedNumber);
+    if (guessedNumbers.length === 1) {
+        guesses.textContent = "Previous guesses:";
+    }
+    guesses.textContent = `${guesses.textContent} ${guessedNumber},`;
+    console.log(guessedNumbers);
     document.getElementById("number-input").value = "";
     document.getElementById("number-input").focus();
     console.log(guessedNumber);

618e1187171ba04084410b0980d8b12291d8a92e
Dana Holm <dana.holm84@gmail.com>
2024-07-03 09:48:18 +0200

    fixed focus on input field, removing gussed number from input field after cubmittin previous guess

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let randomizedNumber;
 let guessedNumber;
 
-function getGuessedNumberVal() {
-    guessedNumber = document.getElementById("number-input").value;
-    console.log(guessedNumber);
-}
-
 function randomizeNumber() {
     randomizedNumber = Math.floor(Math.random() * 100) + 1;
 }
-
 randomizeNumber();
 console.log(randomizedNumber);
+
+function getGuessedNumberVal() {
+    guessedNumber = Number(document.getElementById("number-input").value);
+    checkNumber(guessedNumber);
+    document.getElementById("number-input").value = "";
+    document.getElementById("number-input").focus();
+    console.log(guessedNumber);
+}
+
+function checkNumber(guessedNumber) {
+    if (guessedNumber < randomizedNumber) {
+        document.getElementById("hint").innerHTML = "The number is too low!";
+    } else if (guessedNumber > randomizedNumber) {
+        document.getElementById("hint").innerHTML = "The number is too high!";
+    } else {
+        document.getElementById("hint").innerHTML = "You guessed the number!";
+    }
+}

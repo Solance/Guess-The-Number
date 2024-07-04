67901186fb109efaec52ca1e7e8e6aa783b435c3
Dana Holm <dana.holm84@gmail.com>
2024-07-04 10:27:39 +0200

    the same number can't be guessed twice, added conditions for game over

--- script.js
+++ script.js
@@ -11,24 +11,44 @@ console.log(randomizedNumber);
 
 function submitGuessedNumber() {
     guessedNumber = Number(document.getElementById("number-input").value);
+
     checkNumber(guessedNumber);
-    guessedNumbers.push(guessedNumber);
-    if (guessedNumbers.length === 1) {
+
+    if (guessedNumbers.length === 0) {
         guesses.textContent = "Previous guesses:";
     }
-    guesses.textContent = `${guesses.textContent} ${guessedNumber},`;
-    console.log(guessedNumbers);
+
+    guesses.textContent =
+        guesses.textContent +
+        " " +
+        (guessedNumbers.includes(guessedNumber) ? "" : guessedNumber);
+
+    if (!guessedNumbers.includes(guessedNumber)) {
+        guessedNumbers.push(guessedNumber);
+        console.log(guessedNumbers);
+    }
+
     document.getElementById("number-input").value = "";
     document.getElementById("number-input").focus();
-    console.log(guessedNumber);
 }
 
 function checkNumber(guessedNumber) {
-    if (guessedNumber < randomizedNumber) {
+    if (guessedNumbers.length > 8 && guessedNumber != randomizedNumber) {
+        document.getElementById("hint").innerHTML =
+            "You have reached the maximum of 10 guesses! Maybe next time :)";
+        gameOver();
+    } else if (guessedNumbers.includes(guessedNumber)) {
+        document.getElementById("hint").innerHTML =
+            "You have already tried that number, please enter a different one!";
+    } else if (guessedNumber < randomizedNumber) {
         document.getElementById("hint").innerHTML = "The number is too low!";
     } else if (guessedNumber > randomizedNumber) {
         document.getElementById("hint").innerHTML = "The number is too high!";
     } else {
         document.getElementById("hint").innerHTML = "You guessed the number!";
+        gameOver();
     }
+}
+function gameOver() {
+    console.log("game over!");
 }
